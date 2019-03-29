af76b6ca2fd147c1f765d9db2b41a1e741d2bb9c
Richard Li <richardli125@gmail.com>
2019-03-29 15:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function inc(n) {
 function dec(n) {
   return --n;
 }
-function makeInt(n) {
-  return parseInt(n);
+function makeInt(n, base) {
+  return parseInt(n, base);
 }
 function preserveDecimal(n) {
   return parseFloat(n);
